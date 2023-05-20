e4d79132d13f06840bc0e5ed3047c265057351ca
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2023-05-20 17:34:39 +0200

    first version of the two main archetypes

---
+++ maven-archetype-withparent/src/main/resources/archetype-resources/src/test-integration/java/AppIT.java
@@ -0,0 +1,20 @@
+package $package;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+class AppIT
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
